acdb0d3d16d55e815024f27804c3315be8d83c46
Guillermo Vargas <gavargas22@gmail.com>
2013-04-16 17:08:37 -0600

    Latest working version

--- page-prospective.php
+++ page-prospective.php
@@ -11,6 +11,15 @@
                     Admissions, one of the most visited parts of our website.</p>
             </div>
             <div class="row" id="page-items">
+            	<div class="span4">
+                     <h3>Undergraduate Programs</h3> 
+                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus
+                        ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo
+                        sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed
+                        odio dui.</p>
+                    <p><a class="btn" href="sub-page.html">View details »</a>
+                    </p>
+                </div>
                 <div class="span4">
                      <h3>Graduate Programs</h3> 
                     <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus
