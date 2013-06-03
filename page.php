44bf93778b9fb972e26f5340941fd9913859eb89
Guillermo <gavargas22@gmail.com>
2013-06-03 16:17:11 -0600

    Created the Page template for a page without any specific name

---
+++ page.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+<div class="container" id="single-page">
+	<?php if ( function_exists('yoast_breadcrumb') ) { yoast_breadcrumb('<ul class="breadcrumb">','</ul>'); } ?>
+            
+            <div class="container">
+            	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            		<h2><?php the_title(); ?></h2>
+            		<p><?php the_content(); ?></p>
+            	<?php endwhile; else: ?>
+				<?php endif; ?>
+            </div>
+</div>
+<?php get_footer(); ?>
