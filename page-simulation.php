bdd77dea006cfa6fcfa335fbfa6057f3d14f99a4
Guillermo <gavargas22@gmail.com>
2013-09-26 12:00:23 -0600

    Latest working version

--- page-simulation.php
+++ page-simulation.php
@@ -38,4 +38,55 @@
 </div>
 <?php get_footer(); ?>
 
-
+<?php get_header(); ?>
+<div class="container" id="single-page">
+	<?php if ( function_exists('yoast_breadcrumb') ) { yoast_breadcrumb('<ul class="breadcrumb">','</ul>'); } ?>
+		
+			<?php get_template_part( 'alert-part' ); ?>
+			
+			<div class="hero-unit" id="single-page-teaser" style="background-image:url(<?php bloginfo('stylesheet_directory'); ?>/img/bg/03.jpg); color:#ffffff; background-size:cover; background-position: 50% 100%;">
+         		<h1>Academic Advising</h1>
+         		<p><i>Office of Student Affairs</i></p> 
+        		<p>The Office of Student Affairs for the School of Nursing provides academic advising and highlights educational resources for 
+        			current and prospective students. Our goal is to assist you in attaining your academic goals in a timely manner.</p>
+    		</div>
+    		
+    		<div class="row">
+    			<div class="span9" id="page-items">
+    				<div class="span4">
+                     <h3>Applications and Deadlines</h3> 
+                    <p>Applications will only be available during application windows.</p>
+                    <p><a class="btn btn-primary" href="apps">Applications »</a>
+                    </p>
+                	</div>
+                	<div class="span4">
+                     <h3>Degree Programs</h3> 
+                    <p>The School of Nursing offers a variety of BSN, MSN and PhD tracks that are right for you.</p>
+                    <p><a class="btn btn-primary" href="prospective">View Programs »</a>
+                    </p>
+                	</div>
+                	<div class="span4">
+                     <h3>Office of Student Affairs Directory</h3> 
+                    <p>The knowledgeable staff at the Office of Student Affairs will help you identify the program that is right for you.</p>
+                    <p><a class="btn btn-primary" href="sa-directory">Meet our Staff »</a>
+                    </p>
+                	</div>
+                	<div class="span4">
+                     <h3>Helpful Resources</h3> 
+                    <p>Our goal is to provide our future students with the tools and resources they need to apply and succeed in the UTEP School of Nursing.</p>
+                    <p><a class="btn btn-primary" href="sa-helpful-resources">View details »</a>
+                    </p>
+                	</div>
+    			</div>
+    			<div class="span3">
+    				<h2>Important Links</h2>
+            	<div id="sidebar" class="well">
+            		<?php wp_nav_menu( array( 'menu'=>'Student Affairs Important Links Page', 'items-_wrap' => '%3$s', 'container' => false, 'menu_class' => 'nav nav-pills nav-stacked' )); ?>
+            	</div>
+            	<!--<div class="span3">
+            		Information
+            	</div>-->
+    			</div>
+            </div>
+</div>
+<?php get_footer(); ?>
