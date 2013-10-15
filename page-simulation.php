de477c6425819c879f3f4921057cb5f30e4c73ad
Guillermo <gavargas22@gmail.com>
2013-10-15 13:55:00 -0600

    Nursing Page

--- page-simulation.php
+++ page-simulation.php
@@ -34,24 +34,6 @@
     <div class="row">
     	<div class="span9">
     	<div class="simlab-top-items-wrapper">
-    		<!--<div class="simlab-top-items-element">
-    			<div class="simlab-top-items-element-title">Calendar of Events</div>
-    			<div class="simlab-top-items-element-calendar-wrapper">
-    				<div class="simlab-top-items-element-todays-date">Today: October 10, 2013</div>
-    				<div class="simlab-top-items-element-calendar-appointment">
-    					<div class="simlab-top-items-element-calendar-appointment-title"><a href="#">Art Installation in the SimLab</a></div>
-    					<div class="simlab-top-items-element-calendar-appointment-date">October 42, 2023</div>
-    				</div>
-    				<div class="simlab-top-items-element-calendar-appointment">
-    					<div class="simlab-top-items-element-calendar-appointment-title"><a href="#">Simulated Hospital Day</a></div>
-    					<div class="simlab-top-items-element-calendar-appointment-date">November 22, 2023</div>
-    				</div>
-    				<div class="simlab-top-items-element-calendar-appointment">
-    					<div class="simlab-top-items-element-calendar-appointment-title"><a href="#">SimLab Open House</a></div>
-    					<div class="simlab-top-items-element-calendar-appointment-date">January 14, 2024</div>
-    				</div>
-    			</div>
-    		</div>-->
     		<div class="simlab-top-items-element">
     			<div class="simlab-top-items-element-title">Video 1</div>
     			<div class="simlab-top-items-element-content-wrapper">
@@ -148,9 +130,8 @@
     	</div>
     	<div class="span3">
     		<div class="simlab-top-items-element">
-    			<div class="simlab-top-items-element-title">Important Links</div>
+    			<div class="simlab-top-items-element-title-important-links">Important Links</div>
     			<div class="simlab-top-items-element-calendar-wrapper">
-    				<div class="simlab-top-items-element-todays-date">Today: October 10, 2013</div>
     				<div class="simlab-top-items-element-calendar-appointment">
     					<div class="simlab-top-items-element-calendar-appointment-title"><a href="#">Academic Coaching</a></div>
     				</div>
@@ -173,71 +154,5 @@
     		</div>
     	</div>
     </div>
-    
-    <!--<div class="container">
-    	<div class="row">
-            <div class="span8">
-            	<?php query_posts(array('showposts' => 10, 'orderby' => 'desc', 'category_name' => 'simulation')); ?>
-                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-                	<div class="span7 simlab-elements">
-                		<h2><?php the_title(); ?></h2>
-                		<p><?php the_content(); ?></p>
-                		<p><a href="<?php the_title(); ?>" class="btn btn-small">Read More</a></p>
-                		<?php edit_post_link( 'Edit'); ?>
-                	</div>
-                <?php endwhile; else: ?>
-                <?php endif; ?>
-            </div>
-            <div class="span4">
-            	<div style="padding-left:30px;"><h2>Important Links</h2></div>
-            	<div class="span3">
-                	<h4>Community Partners</h4> 
-                	<p></p>
-                	<p><a class="btn btn-primary btn-small" href="sa-helpful-resources">View details »</a>
-                	</p>
-            	</div>
-            	<div class="span3">
-                	<h4>Simulated Hospital Day</h4> 
-                	<p></p>
-                	<p><a class="btn btn-primary btn-small" href="prospective">View Programs »</a></p>
-            	</div>
-            	<div class="span3">
-                	<h4>ACE</h4> 
-                	<p></p>
-                	<p><a class="btn btn-primary btn-small" href="prospective">View Programs »</a></p>
-            	</div>
-            	<div class="span3">
-                	<h4>Mine Shaft</h4> 
-                	<p></p>
-                	<p><a class="btn btn-primary btn-small" href="prospective">View Programs »</a></p>
-            	</div>
-            	<div class="span3">
-                	<h4>Tours</h4> 
-                	<p></p>
-                	<p><a class="btn btn-primary btn-small" href="prospective">View Programs »</a></p>
-            	</div>
-            	<div class="span3">
-                	<h4>Open Lab</h4> 
-                	<p></p>
-                	<p><a class="btn btn-primary btn-small" href="prospective">View Programs »</a></p>
-            	</div>
-            	<div class="span3">
-                	<h4>Academic Coaching</h4> 
-                	<p></p>
-                	<p><a class="btn btn-primary btn-small" href="prospective">View Programs »</a></p>
-            	</div>
-                <!--<div class="span4" style="margin-left:0;">
-                    <img src="http://orspprofile.utep.edu/profilesystem/images/0/1934_0_2070.jpg" class="img-polaroid" alt="Mr. Ronnie Stout">
-                    <p><address>
-  					<strong>Mr. Ronnie Stout</strong><br>
-  					Family Nurse Practitioner Director/Advisor<br />
-  					<a href="mailto:rcstout@utep.edu">rcstout@utep.edu</a><br />
-  					(915)747-8204   <br />
-					</address>
-                    </p>
-             </div>-->
-            <!--</div>
-        </div>
-    </div>-->
 </div>
 <?php get_footer(); ?>
